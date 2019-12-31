a4c0aedba3f43fb448f301408b3705652a26d308
ioanz <ioan.zicu94@gmail.com>
2019-12-31 20:09:33 +0100

    setup browser / internet intent

--- app/src/main/java/android/example/com/quakereport/Earthquake.java
+++ app/src/main/java/android/example/com/quakereport/Earthquake.java
@@ -7,6 +7,8 @@ public class Earthquake {
     private double mMagnitude;
     private String mLocation;
     private long mTimeInMilliseconds;
+
+    /** Website URL of the earthquake */
     private String mUrl;
 
     /**
@@ -16,7 +18,7 @@ public class Earthquake {
      * @param location is the location where the earthquake happened
      * @param timeInMilliseconds is the time in milliseconds (from the Epoch) when the
      *                           earthquake happened
-     * @param url
+     * @param url is the website URL to find more details about the earthquake
      */
     public Earthquake(double magnitude, String location, long timeInMilliseconds, String url) {
         this.mMagnitude = magnitude;
--- app/src/main/java/android/example/com/quakereport/EarthquakeActivity.java
+++ app/src/main/java/android/example/com/quakereport/EarthquakeActivity.java
@@ -1,5 +1,9 @@
 package android.example.com.quakereport;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,17 +21,39 @@ public class EarthquakeActivity extends AppCompatActivity {
         setContentView(R.layout.earthquake_activity);
 
         // Create a fake list of earthquake locations.
-        ArrayList<Earthquake> earthquakes = QueryUtils.extractEarthquakes();
+        final ArrayList<Earthquake> earthquakes = QueryUtils.extractEarthquakes();
 
         // Find a reference to the {@link ListView} in the layout
-        ListView earthquakeListView = findViewById(R.id.list);
+        final ListView earthquakeListView = findViewById(R.id.list);
 
-        // Create a new {@link ArrayAdapter} of earthquakes
+        // Set the adapter on the {@link ListView}
+        // so the list can be populated in the user interface
         EarthquakeAdapter adapter = new EarthquakeAdapter(
                 this, earthquakes);
 
         // Set the adapter on the {@link ListView}
         // so the list can be populated in the user interface
         earthquakeListView.setAdapter(adapter);
+
+        // Set a click listener to play the audio when the list item is clicked on listView
+        earthquakeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Get the {@link Earthquake} object at the given position the user clicked on
+                Earthquake earthquake = earthquakes.get(position);
+
+                // Setup Browser Intent
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW);
+
+                // Get Earthquake URL
+                String url = earthquake.getUrl();
+
+                // Setup URI parameter
+                browserIntent.setData(Uri.parse(url));
+
+                // Start Activity
+                startActivity(browserIntent);
+            }
+        });
     }
 }
